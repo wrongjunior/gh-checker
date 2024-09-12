4216ffcaa9c6d19801ef7d4c5de46d2ce7f37313
Daniil Solovei <gotodeeps@gmail.com>
2024-09-12 15:11:05 +0700

    fix: update logic work with db

--- internal/services/follower_service.go
+++ internal/services/follower_service.go
@@ -6,35 +6,32 @@ import (
 	"time"
 )
 
-func UpdateFollowers(checkInterval int) {
-	for {
-		followersToCheck, err := database.GetFollowersToCheck(checkInterval)
-		if err != nil {
-			log.Printf("Error getting followers to check: %v", err)
-			time.Sleep(time.Duration(checkInterval) * time.Second)
-			continue
-		}
-
-		for _, follower := range followersToCheck {
-			followers, err := GetFollowers(follower)
-			if err != nil {
-				log.Printf("Error getting followers for %s: %v", follower, err)
-				continue
-			}
-
-			for _, followed := range followers {
-				err := database.AddFollower(follower, followed)
-				if err != nil {
-					log.Printf("Error adding follower %s -> %s: %v", follower, followed, err)
-				}
-			}
-
-			err = database.UpdateLastChecked(follower)
-			if err != nil {
-				log.Printf("Error updating last checked for %s: %v", follower, err)
-			}
-		}
-
-		time.Sleep(time.Duration(checkInterval) * time.Second)
+func UpdateFollowers(username string, updateInterval time.Duration) error {
+	shouldUpdate, err := database.ShouldUpdateFollowers(username, updateInterval)
+	if err != nil {
+		return err
 	}
+
+	if !shouldUpdate {
+		return nil
+	}
+
+	followers, err := GetFollowers(username)
+	if err != nil {
+		return err
+	}
+
+	err = database.ClearFollowers(username)
+	if err != nil {
+		return err
+	}
+
+	for _, follower := range followers {
+		err := database.AddFollower(username, follower)
+		if err != nil {
+			log.Printf("Error adding follower %s -> %s: %v", follower, username, err)
+		}
+	}
+
+	return database.UpdateLastChecked(username)
 }
