3c755f4d221cf9c4896b75d38406c753ba5955c5
Daniil Solovei <gotodeeps@gmail.com>
2024-09-12 15:11:16 +0700

    fix: update logic work with db

--- internal/services/github.go
+++ internal/services/github.go
@@ -3,7 +3,7 @@ package services
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
@@ -17,60 +17,17 @@ func SetGitHubAPIKey(apiKey string) {
 
 func CheckIfFollowing(follower, followed string) (bool, error) {
 	url := fmt.Sprintf("%s/users/%s/following/%s", githubAPI, follower, followed)
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return false, err
-	}
-
-	req.Header.Set("Accept", "application/vnd.github.v3+json")
-
-	if githubAPIKey != "" {
-		req.Header.Set("Authorization", "token "+githubAPIKey)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return false, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusNoContent {
-		return true, nil
-	} else if resp.StatusCode == http.StatusNotFound {
-		return false, nil
-	} else {
-		body, _ := ioutil.ReadAll(resp.Body)
-		return false, fmt.Errorf("GitHub API error: %s", string(body))
-	}
+	return makeGitHubRequest(url)
 }
 
 func GetFollowers(username string) ([]string, error) {
 	url := fmt.Sprintf("%s/users/%s/followers", githubAPI, username)
 
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Accept", "application/vnd.github.v3+json")
-
-	if githubAPIKey != "" {
-		req.Header.Set("Authorization", "token "+githubAPIKey)
-	}
-
-	resp, err := client.Do(req)
+	resp, err := makeGitHubAPIRequest(url)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		body, _ := ioutil.ReadAll(resp.Body)
-		return nil, fmt.Errorf("GitHub API error: %s", string(body))
-	}
 
 	var followers []struct {
 		Login string `json:"login"`
@@ -86,4 +43,37 @@ func GetFollowers(username string) ([]string, error) {
 	}
 
 	return followerNames, nil
+}
+
+func makeGitHubRequest(url string) (bool, error) {
+	resp, err := makeGitHubAPIRequest(url)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusNoContent {
+		return true, nil
+	} else if resp.StatusCode == http.StatusNotFound {
+		return false, nil
+	} else {
+		body, _ := io.ReadAll(resp.Body)
+		return false, fmt.Errorf("GitHub API error: %s", string(body))
+	}
+}
+
+func makeGitHubAPIRequest(url string) (*http.Response, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+
+	if githubAPIKey != "" {
+		req.Header.Set("Authorization", "token "+githubAPIKey)
+	}
+
+	return client.Do(req)
 }
