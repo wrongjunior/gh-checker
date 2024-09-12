db154ff939b0aabf87e93c9ea4d7cb31cdce5abf
Daniil Solovei <gotodeeps@gmail.com>
2024-09-12 14:09:09 +0700

    feat: add load config

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
+	"gh-checker/internal/config"
 	"gh-checker/internal/handlers"
+	"gh-checker/internal/services"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"net/http"
 )
 
 func main() {
+	// Загружаем конфигурацию
+	config.LoadConfig("config.yaml")
+
+	githubAPIKey := config.AppConfig.GitHub.APIKey
+	if githubAPIKey == "" {
+		log.Fatal("GitHub API key not found in config file")
+	}
+
+	services.SetGitHubAPIKey(githubAPIKey)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	// Ручка для подписки
 	r.Post("/api/subscribe", handlers.SubscribeHandler)
 
 	http.ListenAndServe(":8080", r)
