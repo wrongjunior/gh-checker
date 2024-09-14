d9545a1b1dbfa900b17b9a1eb6ec9a5a03cbb629
Daniil Solovei <gotodeeps@gmail.com>
2024-09-14 12:10:29 +0700

    refactor: replace log into logger

--- internal/handlers/subscribe.go
+++ internal/handlers/subscribe.go
@@ -3,27 +3,29 @@ package handlers
 import (
 	"encoding/json"
 	"gh-checker/internal/config"
+	"gh-checker/internal/lib/logger" // Импортируем твой логгер
 	"gh-checker/internal/models"
 	"gh-checker/internal/services"
-	"log"
 	"net/http"
 )
 
+// respondWithError отвечает с ошибкой и логирует её
 func respondWithError(w http.ResponseWriter, err error) {
-	log.Printf("Responding with error: %v", err)
+	logger.Error("Responding with error", err)
 	w.WriteHeader(http.StatusInternalServerError)
 	json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
 }
 
+// SubscribeHandler обрабатывает запрос на проверку подписчиков
 func SubscribeHandler(w http.ResponseWriter, r *http.Request) {
 	var req models.SubscribeRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
-		log.Println("Invalid request body")
+		logger.Error("Invalid request body", err)
 		return
 	}
 
-	log.Printf("Received request to check if %s is following %s", req.Follower, req.Followed)
+	logger.Info("Received request to check if " + req.Follower + " is following " + req.Followed)
 
 	followers, updated, err := services.UpdateFollowers(req.Followed, config.AppConfig.FollowerUpdateInterval)
 	if err != nil {
@@ -40,10 +42,13 @@ func SubscribeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if updated {
-		log.Printf("Updated followers for %s from GitHub API", req.Followed)
+		logger.Info("Updated followers for " + req.Followed + " from GitHub API")
 	} else {
-		log.Printf("Used cached followers data for %s", req.Followed)
+		logger.Info("Used cached followers data for " + req.Followed)
 	}
 
-	json.NewEncoder(w).Encode(models.SubscribeResponse{IsFollowing: isFollowing})
+	if err := json.NewEncoder(w).Encode(models.SubscribeResponse{IsFollowing: isFollowing}); err != nil {
+		respondWithError(w, err)
+		return
+	}
 }
